fe4968e597208c6a4a89d2a6362a1e1e9a211500
pbogonko <pbogonko@safaricom.co.ke>
2023-06-02 14:53:23 +0300

    added extra dependencies

--- src/main/java/org/safaricom/Squad.java
+++ src/main/java/org/safaricom/Squad.java
@@ -1,4 +1,37 @@
 package org.safaricom;
 
 public class Squad {
+    private int maxSize;
+    private String name;
+    private String cause;
+
+    public Squad(int maxSize, String name, String cause) {
+        this.maxSize = maxSize;
+        this.name = name;
+        this.cause = cause;
+    }
+
+    public int getMaxSize() {
+        return maxSize;
+    }
+
+    public void setMaxSize(int maxSize) {
+        this.maxSize = maxSize;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCause() {
+        return cause;
+    }
+
+    public void setCause(String cause) {
+        this.cause = cause;
+    }
 }

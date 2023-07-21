3dbbd7276948a7813d06b53c2616a781a6823855
pbogonko <pbogonko@safaricom.co.ke>
2023-07-21 10:24:46 +0300

    added more changes for db access

--- src/main/java/org/heroSquad/dao/WeaknessDao.java
+++ src/main/java/org/heroSquad/dao/WeaknessDao.java
@@ -1,4 +1,40 @@
 package org.heroSquad.dao;
 
+import org.heroSquad.config.DatabaseConfig;
+import org.heroSquad.models.Weakness;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
+
 public class WeaknessDao {
-}
+
+        private static final Sql2o sql2o = DatabaseConfig.getDatabase();
+
+        public static void create(Weakness weakness){
+            try(Connection connection = sql2o.open()){
+                String query = "INSERT INTO strengths (name, score) VALUES (:name, :score);";
+                connection.createQuery(query)
+                        .addParameter("name", weakness.getName())
+                        .addParameter("score", weakness.getScore())
+                        .executeUpdate();
+            } catch (Exception exception){
+                System.out.println(exception.getMessage());
+            }
+        }
+
+        public static int getScoreById(int id){
+            try(Connection connection = sql2o.open()){
+                String query = "SELECT score FROM strengths WHERE id = :id;";
+                int score = connection.createQuery(query)
+                        .addParameter("id", id)
+                        .executeScalar(Integer.class);
+                System.out.println(score);
+                return 0;
+            } catch (Exception exception){
+                System.out.println(exception.getMessage());
+                return 0;
+            }
+        }
+
+
+    }
+
--- src/main/java/org/heroSquad/utils/SharedUtils.java
+++ src/main/java/org/heroSquad/utils/SharedUtils.java
@@ -1,5 +1,15 @@
 package org.heroSquad.utils;
 
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+import java.util.Map;
+
 public class SharedUtils {
+
+    public static String render(Map<Integer, Object> model, String templateName) {
+        return new HandlebarsTemplateEngine().render(new ModelAndView(model, templateName));
+    }
+
 
 }
